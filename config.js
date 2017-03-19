e8c81248f30aa4d987294b1ed9c9e61fdf4efae8
jiangzhenfei <937361926@qq.com>
2017-03-19 12:06:44 +0800

    time

--- config.js
+++ config.js
@@ -4,14 +4,19 @@ var app=angular.module('app',['ui.router','homeApp','classApp'])
 app.config(function($stateProvider,$urlRouterProvider){
 	$stateProvider.
 	state('home',{
-		url:'/home',
+		url:'/home/:id',
 		//这是首页的组件，要先在模型中注入，也就是homeApp
 		template:'<home-component></home-component>',
 	}).
 	state('class',{
-		url:'/class',
+		url:'/class/:classId',
 		//这是分类页的组件，要先在模型中注入，也就是classAPP
 		template:'<class-component></class-component>',
 	})
 	$urlRouterProvider.otherwise('/home')//默认的加载哪个页面
+})
+app.controller('nav',function($scope,$state){
+	console.log($state)
+	console.log($state.includes('home'))
+
 })
